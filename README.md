4405a18705c788d93427e2c46287b814c763f17a
freakGE <esebua154@gmail.com>
2022-12-04 10:24:33 +0400

    Added: Pick Suggestions, New Flags

--- README.md
+++ README.md
@@ -1,9 +1,27 @@
 <br>
-<h1 align="center">Dota 2 Counter Items for Heroes</h1>
+<h1 align="center">Dota 2 Hero Counters</h1>
 
 <h2 align="center"><b>About</b></h2>
-<h3 align="center">Script returns information from <a href="https://dota2.fandom.com/wiki/Dota_2_Wiki">Dota 2 Wiki</a> </h3>
-<div align="center"><img src="https://user-images.githubusercontent.com/52050303/203575478-dfd17637-f9f2-41ab-89cc-a77781edb1ba.png" /></div>
+<h3 align="center">Script returns data from <a href="https://dota2.fandom.com/wiki/Dota_2_Wiki">Dota 2 Wiki</a> and <a href="https://dotapicker.com">dotapicker</a></h3>
+
+<div align="center" class="thumbnails-container">
+    <div class="thumbnail heroes">
+        <h3><b>Pick Suggestions</b></h3>
+        <img src="https://user-images.githubusercontent.com/52050303/205476611-7a4cbc5d-683b-48c2-a728-45d4026379b9.png" />
+    </div>
+    <div class="thumbnail items">
+        <h3><b>Item Suggestions</b></h3>
+        <img src="https://user-images.githubusercontent.com/52050303/205475912-39643717-daba-43b8-8e6a-d5f5c99decae.png" />
+    </div>
+</div>
+
+<style>
+    .thumbnails-container {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+</style>
 
 ---
 
@@ -23,18 +41,18 @@ npm install
 
 ### **Flags**
 
-| Flag   | Description                                 |
-| ------ | ------------------------------------------- |
-| **-c** | Returns items with their price              |
-| **-k** | To hide message while not including API key |
+| Flag                 | Description                                    |
+| -------------------- | ---------------------------------------------- |
+| **-items**           | Suggests only items                            |
+| **-heroes**          | Suggests only heroes                           |
+| **-utility**         | Suggests only utilityt heroes. (Default: Core) |
+| **-c**               | Returns items with their price                 |
+| **-k**               | To hide message while not including API key    |
+| **-h** or **--help** | Insturctions...                                |
 
 ### **Usage**
 
 To fully utilize the script's capabilities, obtain an [**STEAM API KEY**](https://steamcommunity.com/dev/apikey), open a terminal within **Dota2-HeroCounters-CLI**, create an **.env** file, and paste the key into the variable **STEAM_API_KEY**.
-
-<!-- To use script full potential I recommend to get an [**STEAM API KEY**](https://steamcommunity.com/dev/apikey), open terminal inside **Dota2-HeroCounters-CLI**, create **.env** file and paste the key in variable **STEAM_API_KEY** -->
-
-<!-- go to root directory () and write -->
 
 ```sh
 echo "STEAM_API_KEY=$YOUR_KEY" >> .env # w/o "$"
@@ -43,13 +61,15 @@ echo "STEAM_API_KEY=$YOUR_KEY" >> .env # w/o "$"
 To run script
 
 ```sh
- node heroCounter.js
+ node heroCounter.js # w/o any argument it will suggest both items and heroes
+ node heroCounter.js -items # suggest only items
+ node heroCounter.js -heroes -utility # only utility heroes
 ```
 
 In case u already write ur API key into **.env** file u can use flag **-c** which returns items with their own price.
 
 ```sh
- node heroCounter.js -c
+ node heroCounter.js -items -c
 ```
 
 <div align="center"><img src="https://user-images.githubusercontent.com/52050303/203608667-c197019e-6bd7-4f86-8ae3-0171bf7b481d.png" /></div>
--- heroCounter.js
+++ heroCounter.js
@@ -1,8 +1,9 @@
 try {
   require.resolve("puppeteer");
   require.resolve("sync-fetch");
-  require.resolve("chalk");
   require.resolve("dotenv");
+  require.resolve("cli-table3");
+  require.resolve("@colors/colors");
 } catch (e) {
   console.log(`Error: ${e.code}`);
   console.log(`To install modules type:`);
@@ -13,10 +14,58 @@ try {
 const readline = require("readline");
 const puppeteer = require("puppeteer");
 const fetchSync = require("sync-fetch");
-const chalk = require("chalk");
+const Table = require("cli-table3");
+const colors = require("@colors/colors");
 require("dotenv").config();
 
 const args = process.argv.slice(2);
+
+if (args.includes("-h") || args.includes("-help")) {
+  const table = new Table({
+    head: ["Flag", "Description"],
+    style: {
+      head: ["green", "bold"],
+    },
+  });
+
+  const instructions = [
+    {
+      flag: "-items",
+      description: "Suggest only items",
+    },
+    {
+      flag: "-heroes",
+      description: "Suggest only heroes",
+    },
+    {
+      flag: "-utility",
+      description: "Suggest only utility heroes. (Default: Core)",
+    },
+    {
+      flag: "-c",
+      description: "Returns items with their price",
+    },
+    {
+      flag: "-k",
+      description: "To hide message while not including API key",
+    },
+    {
+      flag: "-h or --help",
+      description: "Insturctions..",
+    },
+  ];
+
+  instructions.map(item => {
+    table.push([
+      colors.magenta.bold(item.flag),
+      colors.bold.cyan(item.description),
+    ]);
+  });
+
+  console.log(table.toString());
+
+  process.exit();
+}
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -95,6 +144,51 @@ const scrapeCounterItems = async url => {
   return data;
 };
 
+const scrapeCounterHeroes = async (url, role) => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: minimal_args,
+  });
+  const page = await browser.newPage();
+
+  await page.goto(url, { waitUntil: "networkidle2" });
+
+  const noHeroes = (await page.$(".heroSuggestionContainerScroll")) || true;
+
+  if (noHeroes === true) {
+    return [];
+  }
+
+  await page.exposeFunction("role", role);
+
+  await page.waitForSelector(`.heroSuggestionContainerScroll`);
+  const data = await page.evaluate(async () => {
+    const counterHeroes = [];
+
+    const roleIndex = await role();
+
+    const list = document.querySelector(".heroSuggestionContainerScroll")
+      .firstElementChild.children[roleIndex].children[0].children;
+
+    for (var i = 0; i < list.length; i++) {
+      let name = list[i].children[1].textContent.trim().split("\n")[0];
+      let rate = parseFloat(
+        list[i].children[1].children[1].textContent
+          .trim()
+          .split("\n")[0]
+          .slice(1, -1)
+          .replace("+", "")
+      ).toFixed(1);
+
+      counterHeroes.push({ name, rate });
+    }
+    return counterHeroes;
+  });
+
+  await browser.close();
+  return data;
+};
+
 const fetchItems = () => {
   try {
     const response = fetchSync(
@@ -107,36 +201,36 @@ const fetchItems = () => {
   } catch (err) {
     if (args.includes("-c")) {
       console.log(
-        `${chalk.red.bold(`API key is missing!`)} ${chalk.green.bold(
+        `${colors.red.bold(`API key is missing!`)} ${colors.green.bold(
           `You can't see items price without API key.`
         )}`
       );
     }
     if (args.includes("-k")) return;
     console.log(
-      `${chalk.bold.green(
-        `To highlight items, enter your Steam API key into ${chalk.magenta(
+      `${colors.bold.green(
+        `To highlight items, enter your Steam API key into ${colors.magenta(
           ".env"
-        )} file.\nFor API key, you can visit ${chalk.blue.underline(
+        )} file.\nFor API key, you can visit ${colors.blue.underline(
           "(https://steamcommunity.com/dev)"
         )}`
       )}`
     );
     console.log(
-      `${chalk.bold.yellow(
-        `echo "STEAM_API_KEY=${chalk.magenta.bold("KEY")}" >> .env`
+      `${colors.bold.yellow(
+        `echo "STEAM_API_KEY=${colors.magenta.bold("KEY")}" >> .env`
       )}`
     );
     console.log(
-      `${chalk.red.bold(
-        `To hide this message add flag ${chalk.magenta("-k")} to the script`
+      `${colors.red.bold(
+        `To hide this message add flag ${colors.magenta("-k")} to the script`
       )}`
     );
   }
 };
 
 rl.question(
-  `${chalk.green.bold(`Enter Hero Name`)}\n${chalk.magenta.bold(">")} `,
+  `${colors.green.bold(`Enter Hero Name`)}\n${colors.magenta.bold(">")} `,
   answer => {
     const ignoredWords = ["of", "the"];
     let input;
@@ -162,19 +256,83 @@ rl.question(
         ? filtredAnswer
         : filtredAnswer.join("_");
 
+    let noData = false;
+
+    if (
+      (args.includes("-items") && args.includes("-heroes")) ||
+      !args.includes("-items")
+    ) {
+      const url = `https://dotapicker.com/herocounter#!/E_${hero}`;
+
+      const counterHeroes = scrapeCounterHeroes(url, () => {
+        return args.includes("-utility") ? 1 : 0;
+      });
+
+      counterHeroes.then(data => {
+        if (data.length === 0) {
+          noData = true;
+          if (args.includes("-heroes") && !args.includes("-items")) {
+            console.log(
+              `${colors.bold.red(
+                "There is currently no data about this hero."
+              )}`
+            );
+            console.log(`${colors.bold.green("Outputs:")}`);
+            [`Hero: ${hero}`, url].map(item => {
+              console.log(`${colors.bold.magenta("⚫")}${item}`);
+            });
+            process.exit();
+          }
+          return;
+        }
+
+        const table = new Table({
+          head: ["Rate", "Hero"],
+          style: {
+            head: ["green", "bold"],
+          },
+        });
+        data.map(hero => {
+          if (hero.rate <= 0) return;
+          table.push([
+            colors.magenta.bold(hero.rate),
+            colors.bold.cyan(hero.name),
+          ]);
+        });
+        console.log(table.toString());
+      });
+    }
+
+    if (args.includes("-heroes") && !args.includes("-items")) {
+      rl.close();
+      return;
+    }
+
     const url = `https://dota2.fandom.com/wiki/${hero}/Counters`;
     const counterItems = scrapeCounterItems(url);
 
     counterItems.then(data => {
       if (data.length === 0) {
         console.log(
-          `${chalk.bold.red("There is currently no text in this page.")}`
+          `${colors.bold.red("There is currently no data about this hero.")}`
         );
-        console.log(`${chalk.bold.green("Outputs:")}`);
-        [`Hero: ${hero}`, url].map(item => {
-          console.log(`${chalk.bold.magenta("⚫")}${item}`);
+        console.log(`${colors.bold.green("Outputs:")}`);
+
+        if (args.includes("-items") && !args.includes("-heroes")) {
+          [`Hero: ${hero}`, url].map(item => {
+            console.log(`${colors.bold.magenta("⚫")}${item}`);
+          });
+          process.exit();
+        }
+
+        [
+          `Hero: ${hero}`,
+          `https://dotapicker.com/herocounter#!/E_${hero}`,
+          url,
+        ].map(item => {
+          console.log(`${colors.bold.magenta("⚫")}${item}`);
         });
-        return;
+        process.exit();
       }
 
       const items = fetchItems();
@@ -183,7 +341,7 @@ rl.question(
         let counterItemText = counterItem;
 
         if (!items) {
-          console.log(`${chalk.bold.magenta("⚫")}${counterItem}`);
+          console.log(`${colors.bold.magenta("⚫")}${counterItem}`);
           return;
         }
 
@@ -194,7 +352,7 @@ rl.question(
             if (args.includes("-c")) {
               counterItemText = `${counterItemText.replace(
                 itemName,
-                `${chalk.bold.blue(itemName)} ${chalk.bold.yellow(
+                `${colors.bold.blue(itemName)} ${colors.bold.yellow(
                   `🟡${item.cost}`
                 )}`
               )}`;
@@ -203,11 +361,11 @@ rl.question(
 
             counterItemText = `${counterItemText.replace(
               itemName,
-              `${chalk.bold.blue(itemName)}`
+              `${colors.bold.blue(itemName)}`
             )}`;
           }
         });
-        console.log(`${chalk.bold.magenta("⚫")}${counterItemText}`);
+        console.log(`${colors.bold.magenta("⚫")}${counterItemText}`);
       });
     });
     rl.close();
